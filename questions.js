cd0eaac4f76bd34f8e96f40488df8b778c7a0e24
MarceloHKR <107078666+MarceloHKR@users.noreply.github.com>
2024-08-21 18:35:15 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,299 @@
+export default [
+    {
+      question: "Deatte 5-Byou De Battle é um anime onde o protagonista Akira Shiroyanagi é levado para uma especie de Jogos vorazes onde cara participande tem um poder único e seu poder é 'o que o seu adversário achar que o seu poder é'. Qual o motivo que fez com que Akira quisesse continuar participando do  jogo? ",
+      answers: [
+        { option: "Por dinheiro", correct: false },
+        { option: "Porque estava entediado", correct: true },
+        { option: "Para salvar sua família", correct: false },
+        { option: "Pela diversão", correct: false },
+      ],
+    },
+    {
+      question: "Mushoku Tensei: Isekai Ittara Honki Dasu: Qual é o nome do protagonista que reencarna em um novo mundo?",
+      answers: [
+        { option: "Rudeus Greyrat", correct: true },
+        { option: "Eris Boreas", correct: false },
+        { option: "Sylphiette", correct: false },
+        { option: "Paul Greyrat", correct: false },
+      ],
+    },
+    {
+      question: "Akashic Records Of Bastard Magic Instructor: Qual é a verdadeira identidade de Glenn Radars?",
+      answers: [
+        { option: "Um ex-assassino", correct: false },
+        { option: "Um ex-herói", correct: false },
+        { option: "Um ex-mago negro", correct: true },
+        { option: "Um ex-cientista", correct: false },
+      ],
+    },
+    {
+      question: "Gotoubun No Hanayome: Qual das irmãs Nakano inicialmente se recusa a estudar com Futaro Uesugi?",
+      answers: [
+        { option: "Ichika", correct: false },
+        { option: "Nino", correct: true },
+        { option: "Miku", correct: false },
+        { option: "Yotsuba", correct: false },
+      ],
+    },
+    {
+        question: "Kimetsu No Yaiba: Qual é o objetivo de Tanjiro Kamado ao se tornar um caçador de demônios?",
+        answers: [
+            { option: "Transformar sua irmã Nezuko de volta em humana.", correct: true },
+            { option: "Vingar a morte de sua família.", correct: false },
+            { option: "Destruir Muzan Kibutsuji.", correct: false },
+            { option: "Encontrar o responsável pela maldição.", correct: false },
+        ],
+        },
+        {
+        question: "Shinigami Bocchan To Kuro Maid: Por que o protagonista é conhecido como 'Bocchan'?",
+        answers: [
+            { option: "Ele é o filho de uma família rica", correct: true },
+            { option: "Ele foi amaldiçoado por uma bruxa", correct: false },
+            { option: "Ele é um rei", correct: false },
+            { option: "Ele é um príncipe", correct: false },
+        ],
+        },
+        {
+        question: "Bastard!! Ankoku No Hakaishin: Qual é o nome do protagonista que se transforma no poderoso mago Dark Schneider?",
+        answers: [
+            { option: "Lucien Renlen", correct: true },
+            { option: "Gara", correct: false },
+            { option: "Kall-Su", correct: false },
+            { option: "Abigail", correct: false },
+        ],
+        },
+        {
+            question: "Overlord: Qual é o nome da guilda que Ainz Ooal Gown lidera?",
+            answers: [
+                { option: "Nazarick", correct: false },
+                { option: "Ainz Ooal Gown", correct: true },
+                { option: "Dark Side.", correct: false },
+                { option: "Eclipse", correct: false },
+            ],
+            },
+        {
+            question: "Palavras Que Borbulham Como Refrigerante: Qual é o hobby de Cherry, o protagonista masculino?",
+            answers: [
+                { option: "Escrever haikus", correct: true },
+                { option: "Desenhar mangás", correct: false },
+                { option: "Colecionar discos de vinil", correct: false },
+                { option: "Pintar murais.", correct: false },
+            ],
+            },
+        {
+            question: "Mieruko-Chan: Qual é a habilidade que Miko Yotsuya descobre que possui?",
+            answers: [
+                { option: "Ver espíritos e fantasmas", correct: true },
+                { option: "Ler mentes", correct: false },
+                { option: "Controlar o tempo", correct: false },
+                { option: "Invocar demônios", correct: false },
+            ],
+            },
+        {
+            question: "Fumetsu no Anata e: Qual é a missão do ser imortal conhecido como Fushi?",
+            answers: [
+                { option: "Aprender e entender a natureza humana através de suas experiências.", correct: true },
+                { option: "Destruir um mal antigo que ameaça o mundo.", correct: false },
+                { option: "Encontrar outros imortais como ele.", correct: false },
+                { option: "Proteger uma civilização em extinção.", correct: false },
+            ],
+            },
+        {
+            question: "Qual é o nome do shinigami que acompanha Light Yagami em 'Death Note'?",
+            answers: [
+                { option: "Rem", correct: false },
+                { option: "Ryuk", correct: true },
+                { option: "Misa", correct: false },
+                { option: "L", correct: false },
+            ],
+            },
+        {
+            question: "Wandering Witch: The Journey of Elaina: Por que Elaina decide viajar pelo mundo?",
+            answers: [
+                { option: "Para experimentar diferentes culturas e aprender novas magias.", correct: true },
+                { option: "Para encontrar um artefato mágico perdido", correct: false },
+                { option: "Para salvar seus pais de uma maldição", correct: false },
+                { option: "Para fugir de um casamento arranjado", correct: false },
+            ],
+            },
+        {
+            question: "Lycoris Recoil: Qual é a missão principal da organização Lycoris?",
+            answers: [
+                { option: "Combater o crime", correct: false },
+                { option: "Proteger a paz na sociedade", correct: false },
+                { option: "Destruir organizações criminosas", correct: true },
+                { option: "Treinar novos agentes", correct: false },
+            ],
+            },
+        {
+            question: "Shingeki No Kyojin: Qual é o objetivo da expedição além das muralhas?",
+            answers: [
+                { option: "Descobrir a origem dos titãs.", correct: true },
+                { option: "Encontrar recursos para o reino", correct: false },
+                { option: "Resgatar civis presos fora das muralhas.", correct: false },
+                { option: "Matar o Titã Colossal.", correct: false },
+            ],
+            },
+        {
+            question: "Arifureta Shokugyou De Sekai Saikyou: Como Hajime Nagumo se torna um dos seres mais poderosos do novo mundo?",
+            answers: [
+                { option: "Ao consumir monstros e adquirir seus poderes.", correct: true },
+                { option: "Ao descobrir um antigo artefato.", correct: false },
+                { option: "Ao fazer um pacto com um demônio.", correct: false },
+                { option: "Ao ser treinado por um herói lendário.", correct: false },
+            ],
+            },
+        {
+            question: "Tonikawa / Tonikaku Kawaii: Como Tsukasa e Nasa se conheceram pela primeira vez?",
+            answers: [
+                { option: "Em uma loja de conveniência.", correct: false },
+                { option: "Em um acidente de trânsito.", correct: true },
+                { option: "Na escola.", correct: false },
+                { option: "Em um casamento.", correct: false },
+            ],
+            },
+        {
+            question: "Black Clover: Qual é o poder que Asta, o protagonista, possui apesar de ser incapaz de usar magia?",
+            answers: [
+                { option: "Anti-magia", correct: true },
+                { option: "Super força", correct: false },
+                { option: " Visão premonitória", correct: false },
+                { option: "Controle do tempo", correct: false },
+            ],
+            },
+        {
+            question: "Suki Na Ko Ga Megane Wo Wasureta: O que faz Komura se destacar na escola em relação à garota que gosta?",
+            answers: [
+                { option: "Ele sempre ajuda ela a encontrar seus óculos.", correct: true },
+                { option: "Ele é o aluno mais inteligente da sala.", correct: false },
+                { option: "Ele é um ótimo atleta", correct: false },
+                { option: "Ele escreve cartas de amor para ela.", correct: false },
+            ],
+            },
+        {
+            question: "Chiyu Mahou no Machigatta Tsukaikata: Qual é a habilidade especial do protagonista?",
+            answers: [
+                { option: "Magia de cura superpoderosa.", correct: true },
+                { option: "Controle sobre o tempo.", correct: false },
+                { option: "Manipulação da gravidade.", correct: false },
+                { option: "Capacidade de convocar seres poderosos.", correct: false },
+            ],
+            },
+        {
+            question: "Horimiya / Hori-San To Miyamura-Kun: Qual é o segredo de Miyamura que ele esconde de seus colegas de classe?",
+            answers: [
+                { option: "Ele tem piercings e tatuagens.", correct: true },
+                { option: "Ele é rico.", correct: false },
+                { option: "Ele é um gênio em matemática.", correct: false },
+                { option: "Ele tem um irmão gêmeo.", correct: false },
+            ],
+            },
+        {
+            question: "Tio De Outro Mundo / ISEKAI OJISAN: Qual é o poder principal do tio do protagonista depois de voltar do outro mundo?",
+            answers: [
+                { option: "Manipulação de elementos.", correct: true },
+                { option: "Controle mental.", correct: false },
+                { option: "Memórias do outro mundo.", correct: false },
+                { option: "Transformação em monstros.", correct: false },
+            ],
+            },
+        {
+            question: "Jojo: Qual é o nome do poder especial que os personagens de Jojo's Bizarre Adventure utilizam?",
+            answers: [
+                { option: "Stands", correct: true },
+                { option: "Nen", correct: false },
+                { option: "Quirks", correct: false },
+                { option: "Cursed Energy", correct: false },
+            ],
+            },
+        {
+            question: "Akame Ga Kill: O que motiva Tatsumi a se juntar ao grupo de assassinos Night Raid?",
+            answers: [
+                { option: "Lutar contra a corrupção do Império", correct: true },
+                { option: "Vingar a morte de sua família.", correct: false },
+                { option: "Proteger sua vila", correct: false },
+                { option: "Encontrar sua irmã perdida", correct: false },
+            ],
+            },
+        {
+            question: "Olhos De Gato: Qual é o desejo que Miyo Sasaki faz ao encontrar a máscara mágica?",
+            answers: [
+                { option: "Ser invisível", correct: false },
+                { option: " Ler mentes.", correct: false },
+                { option: "Se transformar em um gato.", correct: true },
+                { option: "Voltar no tempo", correct: false },
+            ],
+            },
+        {
+            question: "Jujutsu Kaisen: Quem é o mentor de Yuji Itadori na Escola Técnica de Jujutsu de Tóquio?",
+            answers: [
+                { option: "Nanami Kento", correct: false },
+                { option: "Gojo Satoru", correct: true },
+                { option: "Megumi Fushiguro", correct: false },
+                { option: "Yuta Okkotsu", correct: false },
+            ],
+            },
+        {
+            question: "Bleach: Qual é o nome da espada (Zanpakuto) de Ichigo Kurosaki? ",
+            answers: [
+                { option: "Hyorinmaru", correct: false },
+                { option: "Senbonzakura", correct: false },
+                { option: "Zangetsu", correct: true },
+                { option: "Zabimaru", correct: false },
+            ],
+            },
+        {
+            question: "Kakkou No Iinazuke: Por que Nagi Umino foi trocado ao nascer?",
+            answers: [
+                { option: "Um erro no hospital.", correct: true },
+                { option: "Um plano de vingança.", correct: false },
+                { option: "Um desejo dos pais.", correct: false },
+                { option: "Um acidente natural.", correct: false },
+            ],
+            },
+        {
+            question: "Sentouin, Hakenshimasu!: Qual é a missão principal de Combat Agent Six no novo mundo para onde é enviado?",
+            answers: [
+                { option: "Conquistar o mundo para sua organização.", correct: true },
+                { option: "Salvar o mundo de uma invasão alienígena.", correct: false },
+                { option: "Resgatar uma princesa sequestrada.", correct: false },
+                { option: "Descobrir um artefato mágico perdido.", correct: false },
+            ],
+            },
+        {
+            question: "Tensei Shitara Slime Datta Ken: Qual é a habilidade principal de Rimuru Tempest?",
+            answers: [
+                { option: "Controlar o tempo", correct: false },
+                { option: "Manipular fogo", correct: false },
+                { option: "Invocar demônios", correct: false },
+                { option: "Predador", correct: true },
+            ],
+            },
+        {
+            question: "SHIKKAKUMON NO SAIKYOU KENJA: Qual é o objetivo de Matthias, o protagonista?",
+            answers: [
+                { option: "Derrotar o Rei Demônio", correct: false },
+                { option: "Se tornar o mago mais poderoso.", correct: true },
+                { option: "Reconstruir seu reino.", correct: false },
+                { option: "Vingar-se de seus inimigos", correct: false },
+            ],
+            },
+        {
+            question: "Sword Art Online: Em qual nível do jogo Sword Art Online os jogadores descobrem que não podem deslogar?",
+            answers: [
+                { option: "1º", correct: false },
+                { option: "5º", correct: false },
+                { option: "10º", correct: false },
+                { option: "50º", correct: false },
+            ],
+            },
+        {
+            question: "Fullmetal Alchemist: Brotherhood: Quem é o verdadeiro vilão por trás da criação dos homúnculos?",
+            answers: [
+                { option: "Pai", correct: false },
+                { option: "Dante", correct: false },
+                { option: "Roy Mustang.", correct: false },
+                { option: "Van Hohenheim.", correct: true },
+            ],
+            },
+  ];
